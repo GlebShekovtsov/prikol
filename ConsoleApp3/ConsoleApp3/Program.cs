526a86db07e0a29d283fc21916e2e02f5de7e083
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2021-09-04 10:01:30 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            x:
+            Console.WriteLine("Введите число в десятичной системе");
+            int x;
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Результат: ");
+            Console.WriteLine(Convert.ToString(x, 16).ToUpper());
+            goto x;
+        }
+    }
+}
