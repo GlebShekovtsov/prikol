87c79592db23ca017a58ef1910d71ec5b1b106c8
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2021-09-04 09:57:11 +0300

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            m:
+            Console.WriteLine("Выберите сложность пароля: легкий, сложный");
+            string selection = Console.ReadLine();
+            switch(selection)
+            {
+                case "легкий":
+                    int x;
+                    Random rnd = new Random();
+                    x = rnd.Next(10000000, 99999999);
+                    Console.WriteLine($"Ваш пароль: {x}");
+                    goto m;
+                case "сложный":
+                    string iPass = "";
+                    string[] arr = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z", "b", "c", "d", "f", "g", "h", "j", "k", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "A", "E", "U", "Y", "a", "e", "i", "o", "u", "y" };
+                    Random rnd1 = new Random();
+                    for (int i = 0; i < 30; i = i + 1)
+                    {
+                        iPass = iPass + arr[rnd1.Next(0, 57)];
+                    }
+                    Console.WriteLine(iPass);
+                    goto m;
+            }
+        }
+    }
+}
